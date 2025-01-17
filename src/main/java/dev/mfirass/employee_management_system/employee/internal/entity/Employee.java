347369560d235312a4e3e01482ba5f92d30f70a1
Mohammed Firass <firass.mohammed02@gmail.com>
2025-01-17 19:16:44 +0100

    define employee entity.

---
+++ src/main/java/dev/mfirass/employee_management_system/employee/internal/entity/Employee.java
@@ -0,0 +1,48 @@
+package dev.mfirass.employee_management_system.employee.internal.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "employee")
+public class Employee {
+    @Id
+    @NotBlank(message = "Employee ID is required")
+    @Column(name = "employee_id", columnDefinition = "VARCHAR(36)")
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    private String employeeId;
+
+    @NotBlank(message = "Full name is required")
+    private String fullName;
+
+    @NotBlank(message = "Job title is required")
+    private String jobTitle;
+
+    @NotBlank(message = "Department is required")
+    private String department;
+
+    @NotNull(message = "Hire date is required")
+    private LocalDate hireDate;
+
+    @NotBlank(message = "Employment status is required")
+    private String employmentStatus;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Phone number is required")
+    private String phoneNumber;
+
+    @NotBlank(message = "Address is required")
+    private String address;
+}
+
