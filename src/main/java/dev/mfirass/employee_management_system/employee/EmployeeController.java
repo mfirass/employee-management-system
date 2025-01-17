8b3158c76ee776a5c6ac1e188abd1dc856e664b9
Mohammed Firass <firass.mohammed02@gmail.com>
2025-01-17 19:58:51 +0100

    expose employee CRUD endpoints.

---
+++ src/main/java/dev/mfirass/employee_management_system/employee/EmployeeController.java
@@ -0,0 +1,49 @@
+package dev.mfirass.employee_management_system.employee;
+
+import dev.mfirass.employee_management_system.core.common.utils.ResourceUtils;
+import dev.mfirass.employee_management_system.employee.dto.EmployeeCreateRequest;
+import dev.mfirass.employee_management_system.employee.dto.EmployeeResponse;
+import dev.mfirass.employee_management_system.employee.dto.EmployeeUpdateRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(ResourceUtils.EMPLOYEE_ENDPOINT)
+@RequiredArgsConstructor
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<EmployeeResponse>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> getEmployeeById(@PathVariable("id") String id) {
+        EmployeeResponse employeeResponse = employeeService.getEmployeeById(id);
+        return ResponseEntity.ok(employeeResponse);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeResponse> addEmployee(@RequestBody @Valid EmployeeCreateRequest request) {
+        EmployeeResponse employeeResponse = employeeService.createEmployee(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(employeeResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EmployeeResponse> updateEmployee(@PathVariable String id,
+                                                           @RequestBody @Valid EmployeeUpdateRequest request) {
+        return ResponseEntity.ok(employeeService.updateEmployee(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEmployee(@PathVariable("id") String id) {
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.noContent().build();
+    }
+}
