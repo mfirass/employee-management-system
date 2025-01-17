309e4b964d225b30680e1396d8033bf3d3321260
Mohammed Firass <firass.mohammed02@gmail.com>
2025-01-18 00:21:01 +0100

    expose the search endpoint.

--- src/main/java/dev/mfirass/employee_management_system/employee/EmployeeController.java
+++ src/main/java/dev/mfirass/employee_management_system/employee/EmployeeController.java
@@ -5,10 +5,12 @@ import dev.mfirass.employee_management_system.employee.dto.EmployeeCreateRequest
 import dev.mfirass.employee_management_system.employee.dto.EmployeeResponse;
 import dev.mfirass.employee_management_system.employee.dto.EmployeeUpdateRequest;
 import jakarta.validation.Valid;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -48,4 +50,19 @@ public class EmployeeController {
         employeeService.deleteEmployee(id);
         return ResponseEntity.noContent().build();
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<EmployeeResponse>> searchEmployees(
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String id,
+            @RequestParam(required = false) String department,
+            @RequestParam(required = false) String jobTitle,
+            @RequestParam(required = false) String employmentStatus,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hireDateFrom,
+            @RequestParam(required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate hireDateTo) {
+
+        List<EmployeeResponse> employees = employeeService.searchEmployees(name, id, department, jobTitle, employmentStatus, hireDateFrom, hireDateTo);
+        return ResponseEntity.ok(employees);
+    }
+
 }
